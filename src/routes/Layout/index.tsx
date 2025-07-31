12f24c1aa221ebd0df719371ffc5fb5347817d81
Vinicius de Moraes <168435228+vinibmoraes@users.noreply.github.com>
2025-07-31 09:44:28 -0300

    Themas aplicados na navegação através do layout

--- src/routes/Layout/index.tsx
+++ src/routes/Layout/index.tsx
@@ -1,9 +1,11 @@
 // src/routes/Layout/index.tsx
-import { Box } from "@mui/material";
+import { Box, CssBaseline, ThemeProvider } from "@mui/material";
 import { type ReactNode } from "react";
+import { useLocation } from "react-router-dom";
 import Header from "../../pages/Inicio/components/HeaderMain";
 import Sidebar from "../../pages/Inicio/components/Sidebar";
 import { useLayout } from "./useLayout";
+import themes from "../../themes/ThemesExport"; // importa seu dicionário de temas
 
 type LayoutProps = {
   children: ReactNode;
@@ -11,14 +13,21 @@ type LayoutProps = {
 
 export const Layout = ({ children }: LayoutProps) => {
   const { menuOpen, headerHeight, headerRef, toggleMenu } = useLayout();
+  const location = useLocation();
+  const currentPath = location.pathname;
+
+  const theme = themes[currentPath as keyof typeof themes] || themes["/"]; // fallback para mainTheme
 
   return (
-    <Box>
-      <Header onMenuClick={toggleMenu} headerRef={headerRef} />
-      <Sidebar open={menuOpen} onClose={toggleMenu} headerHeight={headerHeight} />
-      <Box component="main" p={3} mt={`${headerHeight}px`}>
-        {children}
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box>
+        <Header onMenuClick={toggleMenu} headerRef={headerRef} />
+        <Sidebar open={menuOpen} onClose={toggleMenu} headerHeight={headerHeight} />
+        <Box component="main" p={3} mt={`${headerHeight}px`}>
+          {children}
+        </Box>
       </Box>
-    </Box>
+    </ThemeProvider>
   );
 };
